eacafbcfcbdfea10d285c654892bcc363ed49278
Ngina07 <phillis.njoroge@strathmore.edu>
2019-12-02 09:14:38 +0300

    add app and bootstrap to installed apps so they can be used in the project

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'area_app'
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIR=[
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
