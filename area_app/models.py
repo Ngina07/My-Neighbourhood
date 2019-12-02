8db66f957ec3028525edffeff6bd202ec47fe3c2
Ngina07 <phillis.njoroge@strathmore.edu>
2019-12-02 09:43:24 +0300

    add profile class to save users information to database

--- area_app/models.py
+++ area_app/models.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models import Q
+import datetime as dt
 
-# Create your models here.
+
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    description = HTMLField()
+    hood = models.ForeignKey(Hood,on_delete=models.CASCADE)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
