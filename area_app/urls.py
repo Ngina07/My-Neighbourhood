52816bfc49c7a4a500fb334ae3ed4e29e93e883f
Ngina07 <phillis.njoroge@strathmore.edu>
2019-12-02 15:49:00 +0300

    update app urls to include method for new businesses and new notifications

--- area_app/urls.py
+++ area_app/urls.py
@@ -9,8 +9,11 @@ urlpatterns=[
     url(r'^businesses',views.businesses, name='businesses'),
     url(r'^health-services',views.health, name='health'),
     url(r'^notifications',views.notification, name='notifications'),
+    url(r'^new/business$',views.new_business, name='new-business'),
+    url(r'^new/notification$',views.new_notification, name='new-notification'),
     url(r'^create/profile$',views.create_profile, name='create-profile'),
     url(r'^update/profile$',views.update_profile, name='update-profile'),
+    url(r'^search/',views.search_results, name='search_results'),
 ]
 
 if settings.DEBUG:
