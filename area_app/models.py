3e8040fbc5247e754bee13bb452a699dd418ae76
Ngina07 <phillis.njoroge@strathmore.edu>
2019-12-02 12:23:04 +0300

    add notifications class to save the users posts

--- area_app/models.py
+++ area_app/models.py
@@ -28,3 +28,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+class notifications(models.Model):
+    title = models.CharField(max_length=100)
+    notification = HTMLField()
+    priority = models.CharField(max_length=15,choices=Priority,default="Informational")
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    hood = models.ForeignKey(Hood,on_delete=models.CASCADE)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
