89fb2ec464a528104e84e7c7e60f5af3ded1b9d9
Ngina07 <phillis.njoroge@strathmore.edu>
2019-12-02 09:46:04 +0300

    add class to save neightbourhood to database

--- area_app/models.py
+++ area_app/models.py
@@ -5,6 +5,18 @@ from tinymce.models import HTMLField
 from django.db.models import Q
 import datetime as dt
 
+class Hood(models.Model):
+    hood = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hood
+
+    def save_hood(self):
+        self.save()
+
+    @classmethod
+    def hood(cls,Hood):
+        cls.objects.filter(hood=hood).delete()
 
 class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatars/')
