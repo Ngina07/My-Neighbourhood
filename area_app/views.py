d0f4a4056ac20d939a9af5738c2649962f580c9c
Ngina07 <phillis.njoroge@strathmore.edu>
2019-12-02 09:52:38 +0300

    add index method to direct logged in user to home page and otherwise to login page

--- area_app/views.py
+++ area_app/views.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
-from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user=request.user
+        profile =Profile.objects.get(username=current_user)
+    except ObjectDoesNotExist:
+        return redirect('create-profile')
+
+    return render(request,'index.html')
